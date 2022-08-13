68d442d7ab9ecb81b99750dc9ce9942f5280ede1
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2022-08-13 09:16:58 +0300

    fix: config file pom.xml and CandidateGrade(model)

--- src/main/java/com/example/hackathonhei/model/CandidateGrade.java
+++ src/main/java/com/example/hackathonhei/model/CandidateGrade.java
@@ -1,6 +1,6 @@
 package com.example.hackathonhei.model;
 
-import jdk.jfr.Enabled;
+//import jdk.jfr.Enabled;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
